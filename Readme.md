7ba6099327498d68a09839942663a722c370c5ff
Eeli Reilin <eeli@fea.st>
2015-09-18 10:35:09 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,178 @@
+# Seq.js
+
+Functions for working with functions, sequences and collections.
+
+## Documentation
+
+### Functions
+
+#### apply(fn, ...args)
+
+Alias for `fn.apply(context, ...args)`.
+
+```js
+apply(Math.sqrt, 25) // => 5
+```
+
+#### partial(fn, ...args)
+
+Returns a partially applied version of `fn`.
+
+```js
+let add = (a, b) => a + b;
+let add10 = partial(add, 10);
+add10(5); // => 15
+```
+
+#### seq(...fns)
+
+Returns a function that executes function arguments sequantially from left to
+right passing their return value as an argument to the next function.
+
+```js
+let square = n => n * n;
+let cube = seq(square, square);
+cube(2); // => 16
+```
+
+### Collections
+
+#### drop(seq, n = 0)
+
+Returns a sequence of all but the first n elements of the sequence seq.
+
+seq may be an array or a string and n must be an integer.  The result is the
+same type of sequence as seq.
+
+```js
+drop([1, 2, 3, 4], 2); // => [3, 4]
+drop('foobar', 3); // => 'bar'
+```
+
+#### dropWhile(pred, seq)
+
+Returns a sub-sequence of the successive elements of seq for which calling pred
+with that element returns non-nil.
+ 
+pred must be a one-argument function and seq may be an array or a string. The
+result is the same type of sequence as seq.
+
+```js
+dropWhile(isOdd, [1, 2, 3, 4]) // => [2, 3, 4]
+dropWhile(isAlphabetic, 'foo123') // => '123'
+```
+
+#### take(seq, n = 0)
+
+Returns a sequence of the first n elements of seq.
+
+seq may be an array or a string and n must be an integer.  The result is the
+same type of sequence as seq.
+
+```js
+take([1, 2, 3, 4], 2); // => [1, 2]
+take('foobar', 3); // => 'foo'
+```
+
+#### takeWhile(pred, seq)
+
+Returns a sub-sequence of the successive elements of seq for which calling pred
+with that element returns non-nil.
+
+pred must be a one-argument function and seq may be an array, or a string. The
+result is the same type of sequence as seq.
+
+```js
+takeWhile(isOdd, [1, 2, 3, 4]) // => [1]
+takeWhile(isAlphabetic, 'foo123') // => 'foo'
+```
+
+#### each(seq, fn)
+
+Calls fn for each element in seq. If seq is an object, calls fn with key and
+value.
+
+```js
+each([1, 2], print) // => 1 2
+```
+
+#### map(fn, seq)
+
+#### filter(pred, seq)
+
+Returns a list of all the elements in seq for which calling pred with that
+element returns false.
+
+```js
+filter(isOdd, [1, 2, 3, 4]) // => [2, 4]
+filter(isAlphabetic, 'foo123') // => '123'
+```
+
+#### concat(...args)
+
+Returns a collection of all arguments concatenated together.
+
+If args are objects, they are merged from right to left.
+
+```js
+concat([1, 2], [3, 4]) // => [1, 2, 3, 4]
+concat('foo', 'bar') // => 'foobar'
+concat({foo: 2}, {bar: 2}, {foo: 1}) // => {foo: 1, bar: 2}
+```
+
+### Predicates
+
+#### not(pred)
+
+Returns a negated version of a predicate function.
+
+```js
+let isNotDigit = not(isDigit);
+
+isDigit('1') // => true
+isNotDigit('1') // => false
+```
+
+#### isDigit(char)
+
+Returns true if char is a digit, false otherwise.
+
+```js
+isDigit('1') // => true
+isDigit('f') // => false
+```
+
+#### isAlphabetic(char)
+
+Returns true if char is alphabetic, false otherwise.
+
+```js
+isAlphabetic('a') // => true
+isAlphabetic('9') // => false
+```
+
+
+### Transformers
+
+#### id(x)
+
+The identity function. Returns the argument as is.
+
+#### toUpper(str)
+
+Returns an all uppercase version of the argument string. Alias for
+String.prototype.toUpperCase.
+
+#### toLower(str)
+
+Returns an all lowercase version of the argument string. Alias for
+String.prototype.toLowerCase.
+
+#### trim(str)
+
+Returns the argument string without any heading or trailing whitespace. Alias
+for String.prototype.trim.
+
+#### stringify(arg)
+
+Alias for JSON.stringify.
---
+++ seq.js
@@ -0,0 +1,216 @@
+export const predicate = {
+    // Returns a negated version of a predicate function.
+    not: pred => () => !pred.apply(this, arguments),
+    // Returns true if the argument is an even number, false
+    // otherwise.
+    isEven: n => n % 2 === 0,
+    // Returns true if the argument is an odd number, false otherwise.
+    isOdd: n => n % 2 !== 0,
+    // Returns true if char is a digit, false otherwise.
+    isDigit: char => /^\d+$/.test(char),
+    // Returns true if char is alphavetic, false otherwise.
+    isAlphabetic: char => /^[A-z]+$/.test(char)
+};
+
+export const transformer = {
+    // The identity function. Returns the argument as is.
+    id: x => x,
+    // Returns an all uppercase version of the argument string.
+    toUpper: String.prototype.toUpperCase,
+    // Returns an all lowercase version of the argument string.
+    toLower: String.prototype.toLowerCase,
+    // Returns the argument string without any heading or trailing
+    // whitespace.
+    trim: String.prototype.trim,
+    stringify: JSON.stringify
+};
+
+export const reducer = {
+    // Returns the sum of the arguments.
+    add: (a, b) => a + b,
+    sub: (a, b) => a - b,
+    mul: (a, b) => a * b,
+    div: (a, b) => a / b,
+    mod: (a, b) => a % b
+};
+
+
+// Applies fn to rest of the arguments.
+export function apply(fn, ...args) {
+    return fn.apply(this, args);
+}
+
+// Returns a function with partially applied arguments.
+export function partial(fn, ...args) {
+    return (...rest) => fn.apply(this, args.concat(rest));
+}
+
+// Returns a function that executes function arguments sequantially passing
+// their return value as an argument to the next function.
+//
+// var cube = seq(square, square);
+// cube(2); // => 16
+export function seq(...fns) {
+    return result => {
+        for (let i = 0; i < fns.length; i++) {
+            result = fns[i].call(this, result);
+        }
+        return result;
+    };
+}
+
+// Returns a sequence of all but the first n elements of the 
+// sequence seq.
+//
+// seq may be a list, vector or string and n must be an integer. 
+// The result is the same type of sequence as seq.
+export function drop(seq, n = 0) {
+    if (seq instanceof Array) {
+        return seq.slice(n);
+    } else if (typeof seq === 'string') {
+        return seq.substr(n);
+    }
+    return null;
+}
+
+// Returns a sub-sequence of the successive elements of seq for 
+// which calling pred with that element returns non-nil.
+// 
+// pred must be a one-argument function and seq may be an array or
+// a string. The result is the same type of sequence as seq.
+export function dropWhile(pred, seq) {
+    if (seq instanceof Array) {
+        let len = seq.length;
+        for (let i = 0; i < len; i++) {
+            if (!pred(seq[i])) {
+                return seq.slice(i);
+            }
+        }
+        return [];
+    } else if (typeof seq === 'string') {
+        let len = seq.length;
+        for (let i = 0; i < len; i++) {
+            if (!pred(seq.charAt(i))) {
+                return seq.substr(i);
+            }
+        }
+        return '';
+    }
+    return null;
+}
+
+// Returns a sequence of the first n elements of seq.
+//
+// seq may be an array or a string and n must be an integer.  The
+// result is the same type of sequence as seq.
+export function take(seq, n = 0) {
+    if (seq instanceof Array) {
+        return seq.slice(0, n);
+    } else if (typeof seq === 'string') {
+        return seq.substr(0, n);
+    }
+    return null;
+}
+
+// Returns a sub-sequence of the successive elements of seq for 
+// which calling pred with that element returns non-nil.
+//
+// pred must be a one-argument function and seq may be an array, or a
+// string. The result is the same type of sequence as seq.
+export function takeWhile(pred, seq) {
+    if (seq instanceof Array) {
+        let len = seq.length;
+        for (let i = 0; i < len; i++) {
+            if (!pred(seq[i])) {
+                return seq.slice(0, i);
+            }
+        }
+        return [];
+    } else if (typeof seq === 'string') {
+        let len = seq.length;
+        for (let i = 0; i < len; i++) {
+            if (!pred(seq.charAt(i))) {
+                return seq.substr(0, i);
+            }
+        }
+        return '';
+    }
+    return null;
+}
+
+// Calls fn for each element in seq. If seq is an object, calls fn
+// with key and value.
+export function each(seq, fn) {
+    if (seq instanceof Array) {
+        let len = seq.length;
+        for (let i = 0; i < len; i++) {
+            fn(seq[i], i);
+        }
+    } else if (typeof seq === 'object') {
+        let keys = Object.keys(seq);
+        let len = keys.length;
+        for (let i = 0; i < len; i++) {
+            const key = keys[i];
+            const val = seq[key];
+            fn(key, val);
+        }
+    } else if (typeof seq === 'string') {
+        let len = seq.length;
+        for (let i = 0; i < len; i++) {
+            fn(seq.charAt(i), i);
+        }
+    }
+}
+
+// Returns a list of all the elements in seq for which calling 
+// pred with that element returns null.
+export function filter(pred, seq) {
+    if (seq instanceof Array) {
+        let ret = [];
+        let len = seq.length;
+        for (let i = 0; i < len; i++) {
+            let elem = seq[i];
+            if (!pred(elem)) {
+                ret.push(elem);
+            }
+        }
+        return ret;
+    } else if (typeof seq === 'object') {
+        let ret = {};
+        let keys = Object.keys(seq);
+        let len = keys.length;
+        for (let i = 0; i < len; i++) {
+            let key = keys[i];
+            let val = seq[key];
+            if (!pred(key, val)) {
+                ret[key] = val;
+            }
+        }
+        return ret;
+    } else if (typeof seq === 'string') {
+        let ret = '';
+        let len = seq.length;
+        for (let i = 0; i < len; i++) {
+            let char = seq.charAt(i);
+            if (!pred(char)) {
+                ret += char;
+            }
+        }
+        return ret;
+    }
+    return null;
+}
+
+// Returns a collection of all arguments concatenated together.
+//
+// If args are objects, they are merged from right to left.
+export function concat(...args) {
+    if (args[0] instanceof Array) {
+        return Array.concat.apply(this, args);
+    } else if (typeof args[0] === 'object') {
+        return Object.assign.apply(this, [{}].concat(Array.prototype.slice.call(arguments)));
+    } else if (typeof args[0] === 'string') {
+        return String.concat.apply(this, args);
+    }
+    return null;
+}
